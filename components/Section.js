17ec5a07e59a0c23fa8625610220183731bb2d05
JinJT10 <javierjin4@yahoo.com>
2026-08-09 21:11:43 -0400

    Created a Section class that handles adding elements to the DOM and refactored index.js to support it

---
+++ components/Section.js
@@ -0,0 +1,20 @@
+class Section {
+    constructor({items, renderer, containerSelector}) {
+        this._items = items;
+        this._renderer = renderer;
+        this._container = document.querySelector(containerSelector);
+    }
+    renderItems() {
+       this._items.forEach((item) => {
+        this._renderer(item);
+        // Call the renderer, and pass it the item as an argument
+       });
+    }
+
+    addItem(element){
+        this._container.append(element);
+        // Add element to the container
+    }
+} 
+
+export default Section;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,31 @@ import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
+
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = document.forms["add-todo-form"];
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
+
+
+const generateTodo = (data) => {
+  const todo = new Todo(data, "#todo-template");
+  return todo.getView();
+};
+
+const section = new Section({
+  items: initialTodos,
+  renderer: (item) => {
+    const todo = generateTodo(item);
+    section.addItem(todo);
+  },
+  containerSelector: ".todos__list",
+});
+section.renderItems();
+
 
 const formValidator = new FormValidator(validationConfig, addTodoForm);
 formValidator.enableValidation();
@@ -21,10 +40,6 @@ const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
 
-const generateTodo = (data) => {
-  const todo = new Todo(data, "#todo-template");
-  return todo.getView();
-};
 
 addTodoButton.addEventListener("click", () => {
   openModal(addTodoPopup);
@@ -34,10 +49,7 @@ addTodoCloseBtn.addEventListener("click", () => {
   closeModal(addTodoPopup);
 });
 
-const renderTodo = (item) => {
-    const todo = generateTodo(item);
-    todosList.append(todo);
-  };
+
 
 
 addTodoForm.addEventListener("submit", (evt) => {
@@ -52,12 +64,10 @@ addTodoForm.addEventListener("submit", (evt) => {
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
   const values = { name, date, id: uuidv4() };
 
-  renderTodo(values);
+  const todo = generateTodo(values)
+  section.addItem(todo);
   closeModal(addTodoPopup);
   formValidator.resetValidation();
 });
 
 
-initialTodos.forEach((item) => {
-  renderTodo(item);
-});
